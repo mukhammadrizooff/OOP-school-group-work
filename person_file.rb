f96e86525747b5f859c002486fd491eddf114b13
Dennis Akagha <d.akagha20@gmail.com>
2022-04-19 15:10:15 +0100

    Effect changes as per reviewers feedback

--- person_file.rb
+++ person_file.rb
@@ -1,36 +1,22 @@
 class Person
   attr_accessor :id, :name, :age
+  attr_reader :id
 
   def initialize(age, name: 'unknown', parent_permission: true)
-    @id = id
+    @id = rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def getid
-    @id
-  end
-
-  def getname
-    @name
-  end
-
-  def getage
-    @age
   end
 
   # private methods
   def of_age
     @age >= 18
   end
-  private :is_of_age
+  private :of_age
 
   def can_use_services
-    if of_age
-      true
-    else
-      false
-    end
+    is_of_age? || @parent_permission
   end
+  public :can_use_services?
 end
