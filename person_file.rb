c5efa8f582afe01296017be3bfc286558f553c61
Dennis Akagha <d.akagha20@gmail.com>
2022-04-21 10:02:06 +0100

    Add decorator class

--- person_file.rb
+++ person_file.rb
@@ -1,12 +1,21 @@
-class Person
+# require_relative 'decorator_file'
+
+class Person < Nameable
+  # getter & setter
   attr_accessor :name, :age
+  # # getter
   attr_reader :id
 
   def initialize(age, name: 'unknown', parent_permission: true)
+    super()
     @id = rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   # private methods
@@ -19,4 +28,37 @@ class Person
     is_of_age? || @parent_permission
   end
   public :can_use_services?
+end
+
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class BaseDecorator < Nameable
+  attr_accessor :name
+
+  def initialize(name)
+    super()
+    @name = name
+  end
+
+  def correct_name
+    @name.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @name.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    return @name.correct_name[0..9] if @name.correct_name.length > 10
+
+    @name.correct_name
+  end
 end
